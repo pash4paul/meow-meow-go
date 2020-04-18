411f5d007b251c50707458a930686810e6cb5e7c
Pavel Fomin <pash4paul@gmail.com>
2020-04-18 17:20:59 +0300

    Update utils.py

--- crawler/utils.py
+++ crawler/utils.py
@@ -1,12 +1,12 @@
 from typing import Sequence, Generator
 
 
-def chunks_by_size(seq: Sequence, chunk_size: input) -> Generator:
+def chunks_by_size(seq: Sequence, chunk_size: int) -> Generator:
     for i in range(0, len(seq), chunk_size):
         yield seq[i:i+chunk_size]
 
 
-def chunks_by_count(seq: Sequence, chunks_count: input):
+def chunks_by_count(seq: Sequence, chunks_count: int) -> Generator:
     for i in range(chunks_count):
         yield seq[i::chunks_count]
 
